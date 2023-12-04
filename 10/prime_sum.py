0d5b366823d3df2108d5b7167e9915018e360393
Mitch <gw2026@wayne.edu>
2023-12-04 13:58:35 -0500

    Problem 10 finished

---
+++ 10/prime_sum.py
@@ -0,0 +1,24 @@
+import math as m
+
+def primeSieve(num):
+    for i in range(2, m.floor(m.sqrt(num))+1):
+        if(num / i == num//i):
+            return False
+    return True
+
+def main():
+    lo = 1
+    hi = 2000000
+    vals = [True for x in range(lo, hi+1)]
+    for x in range(lo+1, hi):
+        if(vals[x] == True):
+            vals[x] = primeSieve(x)
+        if(vals[x] == True):
+            for y in range(2*x, hi, x):
+                vals[y] = False
+
+    primes = [x[0] for x in enumerate(vals) if x[1]]
+    print(sum(primes)-1)
+    
+if __name__ == "__main__":
+    main()
